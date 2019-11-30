7454db8a8dbf834e20085368ec9728d200b73939
Tijmen Wierenga <tijmen.wierenga@get-e.com>
2019-11-30 14:09:56 +0100

    Implemented commentable interface on article and comment

--- src/Article.php
+++ src/Article.php
@@ -6,16 +6,31 @@ namespace TijmenWierenga\Commenting;
 
 use Ramsey\Uuid\UuidInterface;
 
-final class Article
+final class Article implements Commentable
 {
     private UuidInterface $id;
     private string $title;
     private string $content;
+    /**
+     * @var UuidInterface
+     */
+    private $authorId;
 
-    private function __construct(UuidInterface $id, string $title, string $content)
+    public function __construct(UuidInterface $id, string $title, string $content, UuidInterface $authorId)
     {
         $this->id = $id;
         $this->title = $title;
         $this->content = $content;
+        $this->authorId = $authorId;
+    }
+
+    public function resourceType(): string
+    {
+        return 'article';
+    }
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
     }
 }
