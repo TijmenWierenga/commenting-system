5806a8fbc8611644a69815622b712bc683265d3f
Tijmen Wierenga <t.wierenga@live.nl>
2019-12-01 17:02:49 +0100

    Installed router, PSR7 implementation and response sender

--- public/index.php
+++ public/index.php
@@ -4,4 +4,7 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-echo 'Test';
+use GuzzleHttp\Psr7\Response;
+use function Http\Response\send;
+$response = new Response(200, ['Content-Type' => 'application/json'], json_encode(['name' => 'Tijmen']));
+send($response);
