13756fff9c08a06de4d7148bc29ae76057a1637a
Tijmen Wierenga <t.wierenga@live.nl>
2019-12-01 12:59:05 +0100

    Fixed syntax error

--- src/Exceptions/ModelNotFoundException.php
+++ src/Exceptions/ModelNotFoundException.php
@@ -10,6 +10,6 @@ class ModelNotFoundException extends RuntimeException
 {
     public function __construct(string $fqcn, string $id)
     {
-        parent::__construct(sprintf('Model "%s" with ID "%s" was not found'), $fqcn, $id);
+        parent::__construct(sprintf('Model "%s" with ID "%s" was not found', $fqcn, $id));
     }
 }
