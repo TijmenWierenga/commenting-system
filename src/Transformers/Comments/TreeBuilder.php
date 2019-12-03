3e9b43cd14763cfbbcf528b6794dff73ab61d0f7
Tijmen Wierenga <tijmen.wierenga@get-e.com>
2019-12-03 19:09:45 +0100

    Created tree builder to create readable array tree

---
+++ src/Transformers/Comments/TreeBuilder.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TijmenWierenga\Commenting\Transformers\Comments;
+
+use TijmenWierenga\Commenting\Models\Comment;
+
+final class TreeBuilder
+{
+    /**
+     * @var array|Comment[]
+     */
+    private array $comments;
+
+    public function transform(Comment ...$comments): array
+    {
+        $this->comments = $comments;
+
+        $rootId = $comments[0]->getRootId();
+        $rootComments = array_filter(
+            $comments,
+            fn (Comment $item) => $item->getBelongsToId()->toString() === $rootId->toString()
+        );
+
+        return array_values(array_map([$this, 'createComment'], $rootComments));
+    }
+
+    private function createComment(Comment $comment): array
+    {
+        $comments = array_filter(
+            $this->comments,
+            fn (Comment $item): bool => $comment->getId()->toString() === $item->getBelongsToId()->toString()
+        );
+
+        $comments = array_values(array_map([$this, 'createComment'], $comments));
+
+        return [
+            'uuid' => $comment->getId()->toString(),
+            'content' => $comment->getContent(),
+            'authorId' => $comment->getAuthorId()->toString(),
+            'comments' => $comments
+        ];
+    }
+}
---
+++ tests/Transformers/TreeBuilderTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TijmenWierenga\Tests\Commenting\Transformers;
+
+use PHPUnit\Framework\TestCase;
+use TijmenWierenga\Commenting\Models\Comment;
+use TijmenWierenga\Commenting\Transformers\Comments\TreeBuilder;
+use function TijmenWierenga\Tests\Commenting\Factories\make_article;
+use function TijmenWierenga\Tests\Commenting\Factories\make_user;
+
+final class TreeBuilderTest extends TestCase
+{
+    public function testItBuildsATreeForACommentableWithoutComments(): void
+    {
+        $user = make_user('tijmen');
+        $article = make_article('PHP is awesome', 'What a content', $user->getId());
+        $comment = Comment::newFor($article, $user->getId(), 'Such comment');
+
+        $treeBuilder = new TreeBuilder();
+        $result = $treeBuilder->transform($comment);
+
+        $expected = [
+            [
+                'uuid' => $comment->getId()->toString(),
+                'content' => 'Such comment',
+                'authorId' => $user->getId()->toString(),
+                'comments' => []
+            ]
+        ];
+
+        static::assertEquals($expected, $result);
+    }
+
+    public function testItBuildsARecursiveTree(): void
+    {
+        $user = make_user('tijmen');
+        $article = make_article('PHP is awesome', 'What a content', $user->getId());
+        $comment = Comment::newFor($article, $user->getId(), 'Such comment');
+        $commentOnComment = Comment::newFor($comment, $user->getId(), 'I commented on your comment');
+
+        $treeBuilder = new TreeBuilder();
+        $result = $treeBuilder->transform($comment, $commentOnComment);
+
+        $expected = [
+            [
+                'uuid' => $comment->getId()->toString(),
+                'content' => 'Such comment',
+                'authorId' => $user->getId()->toString(),
+                'comments' => [
+                    [
+                        'uuid' => $commentOnComment->getId()->toString(),
+                        'content' => 'I commented on your comment',
+                        'authorId' => $user->getId()->toString(),
+                        'comments' => []
+                    ]
+                ]
+            ]
+        ];
+
+        static::assertEquals($expected, $result);
+    }
+}
