2f946596d5ce151508f68825d97419d1d407c17a
Tijmen Wierenga <tijmen.wierenga@get-e.com>
2019-12-04 21:06:35 +0100

    Using factory method in model factory (haha)

--- tests/Factories/model_factories.php
+++ tests/Factories/model_factories.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace TijmenWierenga\Tests\Commenting\Factories;
 
-use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 use TijmenWierenga\Commenting\Models\{Article, CommentableId, User};
 
 function make_user(string $username): User
 {
-    return new User(Uuid::uuid4(), $username, '123456');
+    return User::new($username, '123456');
 }
 
 function make_article(string $title, string $content, UuidInterface $authorId): Article
